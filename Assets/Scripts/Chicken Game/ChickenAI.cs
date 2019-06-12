abb67cb201e1a745ece2d6040e4f8158da5575b4
JesseSmart <jesse.oscar.smart@gmail.com>
2019-06-12 15:32:26 +1000

    Worked on chicken mini game, adding chicken ai

---
+++ Assets/Scripts/Chicken Game/ChickenAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour
+{
+    private GameObject[] playerObjs;
+    public float minFleeRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObjs = GameObject.FindGameObjectsWithTag("Player");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        for (int i = 0; i < playerObjs.Length; i++)
+        {
+            float dist = Vector3.Distance(transform.position, playerObjs[i].transform.position);
+
+            if (dist <= minFleeRange)
+            {
+                transform.position = -Vector3.MoveTowards(transform.position, playerObjs[i].transform.position, 0.5f * Time.deltaTime);
+            }
+        }
+    }
+}
