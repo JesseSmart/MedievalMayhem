cc7d9557e15dc50c2f3a34fe3895f6ad872ce97b
JesseSmart <jsmart6@sae.au>
2019-06-14 13:15:18 +1000

    basic posion game character

---
+++ Assets/Scripts/Potion Game/PotionCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionCharacterController : MonoBehaviour
+{
+    public Rigidbody rbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+
+
+        Vector3 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 inputVector = new Vector3(horizontalInput, 0, verticalInput);
+        inputVector = Vector3.ClampMagnitude(inputVector, 1); //1 might have to be baseMovementSpeed instead. clamp prevents diagonal movement being faster
+        Vector3 movement = inputVector * speed;
+        Vector3 newPos = currentPos + movement * Time.fixedDeltaTime;
+        //transform.Translate(newPos);
+        rbody.MovePosition(newPos);
+    }
+}
